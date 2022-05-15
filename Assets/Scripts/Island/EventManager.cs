539b31d06941f15aedd31e26103c613b691a078e
Brani <branislav.uher@student.ukf.sk>
2022-05-15 16:05:08 +0200

    attempt at merging develop and island

--- Assets/Scripts/Island/EventManager.cs
+++ Assets/Scripts/Island/EventManager.cs
@@ -1,54 +1,124 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Random = System.Random;
 
 public class EventManager : MonoBehaviour
 {
     [SerializeField] bool isChallenge = false;
     [SerializeField] EventType eventType;
+    [SerializeField] TextMeshProUGUI  BtnPrompt;
     [SerializeField] private int timeCost = 1;
+    
+    [SerializeField] private Sprite spriteRandom;
+    [SerializeField] private Sprite spriteMerchant;
+    [SerializeField] private Sprite spriteHarvest;
+    [SerializeField] private Sprite spriteChallenge;
+    
+    //EVENT WINDOW
+    [SerializeField] private GameObject  eventWindow;
+
+    [SerializeField] private Sprite spriteEventImg;
+    [SerializeField] private TextMeshProUGUI  eventWindowName;
+    [SerializeField] private TextMeshProUGUI  eventWindowText;
+    [SerializeField] private TextMeshProUGUI eventFirstButtontext;
+    [SerializeField] private TextMeshProUGUI eventSecondButtontext;
+    [SerializeField] private TextMeshProUGUI eventThirdButtontext;
+
+    private IslandManager islandManager;
+    private bool isOnEvent = false;
+    private bool isUsed = false;
+
+    private void Awake()
+    {
+        eventWindow = GameObject.FindGameObjectsWithTag("EventWindow")[0];
+    }
 
     void Start()
     {
-        if (isChallenge) this.eventType = EventType.Challenge;
-        else assignRandomType();
+        //spriteEventImg = eventWindow.GetComponentInChildren()
+
+        AssignType();
+        islandManager = FindObjectOfType<IslandManager>();
+        eventWindow.SetActive(false);
     }
 
-    public void assignRandomType()
+    private void AssignType()
+    {
+        if (isChallenge) this.eventType = EventType.Challenge;
+        else AssignRandomType();
+        AssignSprite();
+    }
+
+    public void AssignRandomType()
     {
         Array values = Enum.GetValues(typeof(EventType));
         Random random = new Random(Guid.NewGuid().GetHashCode());
         this.eventType = (EventType) values.GetValue(random.Next(values.Length - 1));
     }
-
-    void Update()
+    
+    public void AssignSprite()
     {
-    }
-
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        IslandManager islandManager = FindObjectOfType<IslandManager>();
-        islandManager.lowerTime(timeCost);
+        Image imageComponent = GetComponent<Image>();
         
+
         switch (eventType)
         {
             case EventType.Merchant:
-                Debug.Log("Buy shit!");
+                Debug.Log("MERCHANT");
+                imageComponent.sprite  =  spriteMerchant;
                 break;
-            case EventType.Academy:
-                Debug.Log("Learn shit!");
+            case EventType.Random:
+                Debug.Log("RANDOM EVENT");
+                imageComponent.sprite =  spriteRandom;
+
                 break;
-            case EventType.Forest:
-                Debug.Log("Lose shit!");
-                break;
-            case EventType.Lava:
-                Debug.Log("Shit yourself!");
+            case EventType.Harvest:
+                Debug.Log("HARVEST");
+                imageComponent.sprite =  spriteHarvest;
+
                 break;
             case EventType.Challenge:
-                Debug.Log("Betka ma bije!");
+                Debug.Log("CHALLENGE");
+                imageComponent.sprite =  spriteChallenge;
+
+                break;
+            default:
+                Debug.Log("What are you doing here?");
+                break;
+        }
+    }
+    
+    void Update()
+    {
+        if (isOnEvent && !isUsed && Input.GetButtonDown("Jump"))
+        {
+            isUsed = true;
+            ClearBtnPrompt();
+            RecognizeAndRunEvent();
+            islandManager.lowerTime(timeCost);
+            
+            eventWindow.SetActive(true);
+        }
+    }
+    
+    private void RecognizeAndRunEvent()
+    {
+        switch (eventType)
+        {
+            case EventType.Merchant:
+                Debug.Log("MERCHANT");
+                break;
+            case EventType.Random:
+                Debug.Log("RANDOM EVENT");
+                break;
+            case EventType.Harvest:
+                Debug.Log("HARVEST");
+                break;
+            case EventType.Challenge:
+                Debug.Log("CHALLENGE");
                 break;
             default:
                 Debug.Log("What are you doing here?");
@@ -56,12 +126,28 @@ public class EventManager : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(!isUsed) BtnPrompt.text = "Press SPACE to do stuff";
+        isOnEvent = true;
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        ClearBtnPrompt();
+        isOnEvent = false;
+    }
+    
+    private void ClearBtnPrompt()
+    {
+        BtnPrompt.text = "";
+    }
+
     enum EventType : int
     {
         Merchant = 1,
-        Academy = 2,
-        Forest = 3,
-        Lava = 4,
+        Random = 2,
+        Harvest = 3,
 
         Challenge = 10
     }
