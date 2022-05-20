e60fa48cca835e3a71b7a3006a43713ea531505a
MatusValko <mvmatusvalko@gmail.com>
2022-05-21 01:13:11 +0200

    Added random generation of Random event types, event window control and random event texts

--- Assets/Scripts/Island/EventManager.cs
+++ Assets/Scripts/Island/EventManager.cs
@@ -1,15 +1,14 @@
 using System;
-using System.Linq;
 using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Random = System.Random;
 
 public class EventManager : MonoBehaviour
 {
-    [SerializeField] private bool isChallenge = false;
+    [SerializeField] private bool isChallenge;
     [SerializeField] private EventType eventType;
+    [SerializeField] private RandomEventType randomEventType; //FOR NOW
     [SerializeField] private TextMeshProUGUI  btnPrompt;
     [SerializeField] private int timeCost = 1;
     
@@ -21,76 +20,138 @@ public class EventManager : MonoBehaviour
     //EVENT WINDOW
     [SerializeField] private GameObject  eventWindow;
 
-    [SerializeField] private Sprite spriteEventImg;
-    [SerializeField] private TextMeshProUGUI  eventWindowName;
-    [SerializeField] private TextMeshProUGUI  eventWindowText;
-    [SerializeField] private TextMeshProUGUI eventFirstButtontext;
-    [SerializeField] private TextMeshProUGUI eventSecondButtontext;
-    [SerializeField] private TextMeshProUGUI eventThirdButtontext;
-
     private IslandManager islandManager;
     
-    private bool isOnEvent = false;
-    private bool isUsed = false;
+    private bool isOnEvent;
+    private bool isUsed;
     private bool canShowEventWindow = true;
     //private GameObject playerGrabber;
 
+    
     private void Awake()
     {
-        eventWindow = GameObject.FindGameObjectsWithTag("EventWindow")[0];
+        
+        eventWindow = GameObject.FindGameObjectWithTag("EventWindow");
     }
 
     void Start()
     {
         //spriteEventImg = eventWindow.GetComponentInChildren()
 
-        AssignType();
+        AssignRandomType();
         AssignSprite();
-        eventWindow.SetActive(false);
+        eventWindow.GetComponent<EventWindowControl>().HideWindow();
+        
+
         islandManager = FindObjectOfType<IslandManager>();
     }
+
     
-    //Určí typ eventu, Challenge sa nastavuje z inšpektora
-    private void AssignType()
-    {
-        if (isChallenge) eventType = EventType.Challenge;
-        else AssignRandomType();
-    }
-    
-    //Urči náhodne typ eventu
+    //Urči typ eventu, Challenge sa nastavuje z inšpektora
     private void AssignRandomType()
-    {
-        Array values = Enum.GetValues(typeof(EventType));
-        Random random = new Random(Guid.NewGuid().GetHashCode());
-        eventType = (EventType) values.GetValue(random.Next(values.Length - 1));
+    {   
+        if (isChallenge) eventType = EventType.Challenge;
+        else {
+            Array values = Enum.GetValues(typeof(EventType));
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            //TODO: ZMENIT -3 NA -1 PO TESTOVANI RANDOM EVENTOV
+            eventType = (EventType) values.GetValue(random.Next(values.Length - 3));
+        }
     }
+    //Urči náhodný typ  Random eventu
+    private void AssignRandomEventRandomType()
+    {
+        Array rvalues = Enum.GetValues(typeof(RandomEventType));
+        Random random = new Random(Guid.NewGuid().GetHashCode());
+        randomEventType = (RandomEventType) rvalues.GetValue(random.Next(rvalues.Length ));
+    }
+
+    private void SwitchEventWindow()
+    {
+        EventWindowControl eventWindowControl = eventWindow.GetComponent<EventWindowControl>();
+        switch (randomEventType)
+        {
+            case RandomEventType.HomelessCat:
+                Debug.Log("HomelessCat");
+                eventWindowControl.SetUpEventWindow("Homeless cat","You found homeless cat on the street.",
+                    "POMOC","","");
+
+
+                
+                break;
+            case RandomEventType.DiceCat:
+                Debug.Log("DiceCat");
+                eventWindowControl.SetUpEventWindow("Dice Cat","You see a cat playing dice.",
+                    "POMOC","","");
+
+  
+                
+                break;
+            case RandomEventType.Stumble:
+                Debug.Log("Stumble");
+                eventWindowControl.SetUpEventWindow("Stumble","You stumbled on a small rock and lost an ingredient."
+                ,"POMOC","","");
+
+                
+                
+                break;
+            case RandomEventType.PerfectTomatoes:
+                Debug.Log("PerfectTomatoes");
+                eventWindowControl.SetUpEventWindow("Perfect tomatoes","You see perfect tomatoes behind a fence.",
+                    "POMOC","","");
+                
+                break;
+            case RandomEventType.Cave:
+                Debug.Log("Cave");
+                eventWindowControl.SetUpEventWindow("Cave","You see entrance to a cave.",
+                    "POMOCPLS","","LEAVE");
+                break;
+            case RandomEventType.StuckMerchant:
+                Debug.Log("StuckMerchant");
+                eventWindowControl.SetUpEventWindow("Stuck Merchant","You see a stucked merchant on the road.",
+                    "POMOC","","LEAVE");
+                break;
+            case RandomEventType.Thieves:
+                Debug.Log("Thieves");
+                eventWindowControl.SetUpEventWindow("Thieves","You see thieves trying to rob you.",
+                    "POMOC","","LEAVE");
+                break;
+            default:
+                Debug.Log("What are you doing here CRIMINAL SCUM?");
+                break;
+        }
+    }
+
+    
+    
     
     //Priradí sprite podĺa typu eventu
     private void AssignSprite()
     {
         Image imageComponent = GetComponent<Image>();
         
-
         switch (eventType)
         {
             case EventType.Merchant:
-                Debug.Log("MERCHANT");
+                //Debug.Log("MERCHANT");
                 imageComponent.sprite  =  spriteMerchant;
                 break;
             case EventType.Random:
-                Debug.Log("RANDOM EVENT");
+                //Debug.Log("RANDOM EVENT");
                 imageComponent.sprite =  spriteRandom;
 
+                AssignRandomEventRandomType();
+                
+                
+                
                 break;
             case EventType.Harvest:
-                Debug.Log("HARVEST");
+                //Debug.Log("HARVEST");
                 imageComponent.sprite =  spriteHarvest;
-
                 break;
             case EventType.Challenge:
-                Debug.Log("CHALLENGE");
+                //Debug.Log("CHALLENGE");
                 imageComponent.sprite =  spriteChallenge;
-
                 break;
             default:
                 Debug.Log("What are you doing here?");
@@ -107,12 +168,8 @@ public class EventManager : MonoBehaviour
             isUsed = true;
             ClearBtnPrompt();
             RecognizeAndRunEvent();
-
-            if (canShowEventWindow)
-            {
-                eventWindow.GetComponent<EventWindowControl>().Init(timeCost);
-                eventWindow.SetActive(true);   
-            }
+            
+           
         }
     }
     
@@ -126,8 +183,14 @@ public class EventManager : MonoBehaviour
                 break;
             case EventType.Random:
                 Debug.Log("RANDOM EVENT");
-                
-                
+                SwitchEventWindow();
+
+                if (canShowEventWindow)
+                {
+                    eventWindow.GetComponent<EventWindowControl>().ShowWindow();
+                    eventWindow.GetComponent<EventWindowControl>().Init(timeCost);
+                }
+
                 break;
             case EventType.Harvest:
                 Debug.Log("HARVEST");
@@ -161,8 +224,7 @@ public class EventManager : MonoBehaviour
         btnPrompt.text = "";
     }
 
-    enum EventType : int
-    {
+    private enum EventType{
         Random = 1,
         Harvest = 2,
         Merchant = 3,
@@ -170,4 +232,20 @@ public class EventManager : MonoBehaviour
         
         Challenge = 10
     }
+
+    private enum RandomEventType
+    {
+        HomelessCat = 1,
+        DiceCat = 2,
+        Stumble = 3,
+        PerfectTomatoes = 4,
+        Cave = 5,
+        StuckMerchant = 6,
+        Thieves = 7,
+    }
+    
+    
+    
+    
+    
 }
--- Assets/Scripts/Island/EventWindowControl.cs
+++ Assets/Scripts/Island/EventWindowControl.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -17,19 +19,84 @@ public class EventWindowControl : MonoBehaviour
 
     private int timeCost = 1;
 
+    //[SerializeField] private GameObject  eventWindow;
+/*
+    [SerializeField] private Sprite spriteEventImg;
+    [SerializeField] private TextMeshProUGUI  eventWindowName;
+    [SerializeField] private TextMeshProUGUI  eventWindowText;
+    [SerializeField] private TextMeshProUGUI eventFirstButtonText;
+    [SerializeField] private TextMeshProUGUI eventSecondButtonText;
+    [SerializeField] private TextMeshProUGUI eventThirdButtonText;
+*/
+
+    [SerializeField] private GameObject  WindowName;
+    [SerializeField] private GameObject  WindowText;
+    [SerializeField] private GameObject  WindowImage;
+    [SerializeField] private GameObject  FirstButton;
+    [SerializeField] private GameObject  SecondButton;
+    [SerializeField] private GameObject  ThirdButton;
+
+    
+    private void Awake()
+    {
+        //Zbytočné, je to v prefabe
+        //Prehľadá všetky child elementy a priradí ich do premenných
+        /*
+        foreach (Transform child in transform)
+        {
+            switch (child.name)
+            {
+                case "EventName":
+                    WindowName = child.gameObject;
+                    break;
+                case "EventText":
+                    WindowText = child.gameObject;
+                    break;
+                case "EventImage":
+                    WindowImage = child.gameObject;
+                    break;
+                case "FirstButton":
+                    FirstButton = child.gameObject;
+                    break;
+                case "SecondButton":
+                    SecondButton = child.gameObject;
+                    break;
+                case "ThirdButton":
+                    ThirdButton = child.gameObject;
+                    break;
+                default:
+                    Debug.Log("What are you doing here?");
+                    
+                    break;
+            }
+            //Debug.Log(child.name);
+
+        }
+       */
+    }
+
     void Start()
     {
-        fstBtn = transform.Find("FirstButton").GetComponent<Button>();
-        scdBtn = transform.Find("SecondButton").GetComponent<Button>();
-        trdBtn = transform.Find("ThirdButton").GetComponent<Button>();
+        
+        
+        //fstBtn = transform.Find("FirstButton").GetComponent<Button>();
+        //scdBtn = transform.Find("SecondButton").GetComponent<Button>();
+        //trdBtn = transform.Find("ThirdButton").GetComponent<Button>();
 
         islandManager = FindObjectOfType<IslandManager>();
         
         // Takto vyzera pridavanie funkcii buttonom zo scriptu
         //  bude sa hodit, ak sa budu funkcie na buttonoch menit za behu hry, napriklad v reakcii na hracov input
-        fstBtn.onClick.AddListener(delegate { CloseEvent(); });
-        scdBtn.onClick.AddListener(delegate { CloseEvent(); });
-        trdBtn.onClick.AddListener(delegate { CloseEvent(); });
+        //fstBtn.onClick.AddListener(delegate { CloseEvent(); });
+        //scdBtn.onClick.AddListener(delegate { CloseEvent(); });
+        //trdBtn.onClick.AddListener(delegate { CloseEvent(); });
+
+        
+        FirstButton.GetComponent<Button>().onClick.AddListener(delegate { CloseEvent(); });
+        SecondButton.GetComponent<Button>().onClick.AddListener(delegate { CloseEvent(); });
+        ThirdButton.GetComponent<Button>().onClick.AddListener(delegate { CloseEvent(); });
+
+         
     }
 
 
@@ -51,6 +118,55 @@ public class EventWindowControl : MonoBehaviour
         islandManager.lowerTime(timeCost);
         GameObject.FindGameObjectsWithTag("EventWindow")[0].SetActive(false);
     }
+
+    public void SetWindowName(string text)
+    {
+        WindowName.GetComponent<TMP_Text>().text = text;
+    }
+    public void SetWindowText(string text)
+    {
+        WindowText.GetComponent<TMP_Text>().text = text;
+    }
+    public void SetFirstButtonText(string text)
+    {
+        FirstButton.GetComponentInChildren<TMP_Text>().text = text;
+    }
+    public void SetSecondButtonText(string text)
+    {
+        if (string.IsNullOrEmpty(text))
+        {
+            SecondButton.SetActive(false);
+        }
+        else
+        {
+            SecondButton.SetActive(true);
+            SecondButton.GetComponentInChildren<TMP_Text>().text = text;
+        }
+    }
+    public void SetThirdButtonText(string text)
+    {
+        ThirdButton.GetComponentInChildren<TMP_Text>().text = text;
+    }
     
+    public void ShowWindow()
+    {
+        gameObject.SetActive(true);
+        //transform.position += Vector3.forward * 2000;
+    }
+    public void HideWindow()
+    {
+        gameObject.SetActive(false);
+        //transform.position -= Vector3.forward * 2000;
+    }
+    
+    public void SetUpEventWindow(string name,string text,string firstBtn = "",string secondtBtn = "",string thirdtBtn = "")
+    {
+        //EventWindowControl eventWindowControl = eventWindow.GetComponent<EventWindowControl>();
+        SetWindowName(name);
+        SetWindowText(text);
+        SetFirstButtonText(firstBtn);
+        SetSecondButtonText(secondtBtn);
+        SetThirdButtonText(thirdtBtn);
+    }
     
 }
