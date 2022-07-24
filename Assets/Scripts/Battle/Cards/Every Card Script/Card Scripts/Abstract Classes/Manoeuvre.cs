b448f09e17c33ce5bd54ed87f97072335f71197b
Brani <branislav.uher@student.ukf.sk>
2022-07-24 09:58:50 +0200

    Forbid playing discard card if no cards can be discarded

---
+++ Assets/Scripts/Battle/Cards/Every Card Script/Card Scripts/Abstract Classes/Manoeuvre.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Manoeuvre : Card
+{
+    public Manoeuvre()
+    {
+        this.CardType = "Manoeuvre";
+    }
+
+    public abstract bool CanBeUsed();
+}
---
+++ Assets/Scripts/Battle/Cards/Every Card Script/Card Scripts/DiscardCardEffect.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class DiscardCardEffect : CardEffect
+{
+    [SerializeField] protected string[] filter;
+
+    protected async void Discard(GameManager gm, Action discardSuccessfulCallback = null)
+    {
+        // if (CanBeUsed(gm))
+        // {
+            gm.SetDiscardFilter(filter);
+            await gm.StartDiscard();
+            discardSuccessfulCallback?.Invoke();
+        // }
+    }
+
+    /// <summary>
+    /// Default check for whether Discard can be used, can be overriden in child classes
+    /// Checks for given amount of any card by default
+    /// 'virtual' makes it possible to be ovrwritten in child classes and used here
+    /// </summary>
+    /// <param name="gm"></param>
+    /// <returns></returns>
+    public virtual bool CanBeUsed(GameManager gm)
+    {
+        return gm.IsEnoughCardsOnHand(discardAmount);
+    }
+}
